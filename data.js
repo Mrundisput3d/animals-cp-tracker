bf39ae574a24d3d2e4a13dd2fbced82fec1bad6c
Mrundisput3d <geogewrgiou@gmail.com>
2026-03-04 01:08:27 +0200

    Update data.js

--- data.js
+++ data.js
@@ -15,21 +15,25 @@ window.L2_DATA = {
   players: {
     "Mrundisput3d": {
       activities: { siege: 4, valakas: 2, antharas: 1, frintezza: 2, zaken: 1, antQueen: 3, orfen: 1, core: 1 },
-      bought:     { orfen: 1, core: 1 }
+      bought:     { zaken: 1 }
     },
     "Fidempor12": {
       activities: { siege: 6, valakas: 2, antharas: 2, frintezza: 1, zaken: 2, antQueen: 2, orfen: 2, core: 1 },
-      bought:     { core: 1, orfen: 1 }
+      bought:     {  }
     },
     "GRP": {
       activities: { siege: 4, valakas: 3, antharas: 3, frintezza: 1, zaken: 1, antQueen: 2, orfen: 1, core: 1 },
-      bought:     { antQueen: 1 }
+      bought:     {  }
     },
     "Dreamcatcher": {
       activities: { siege: 8, valakas: 1, antharas: 2, frintezza: 3, zaken: 1, antQueen: 1, orfen: 2, core: 3 },
       bought:     { frintezza: 1 }
     },
     "RichWildee": {
+      activities: { siege: 5, valakas: 2, antharas: 2, frintezza: 2, zaken: 3, antQueen: 2, orfen: 2, core: 1 },
+      bought:     {  }
+    },
+    "MONARXIDOS": {
       activities: { siege: 5, valakas: 2, antharas: 2, frintezza: 2, zaken: 3, antQueen: 2, orfen: 2, core: 1 },
       bought:     { zaken: 1 }
     }

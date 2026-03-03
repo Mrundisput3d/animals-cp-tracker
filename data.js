8a1f94d91758c7ee05ef532dfb057253243bee6a
Vasilis Souvatzis <souvatzisbill@gmail.com>
2026-03-04 00:41:27 +0200

    Initial commit

---
+++ data.js
@@ -0,0 +1,37 @@
+window.L2_DATA = {
+  config: {
+    activities: {
+      siege:     { label: "Castle Siege", points: 9 },
+      valakas:   { label: "Valakas",      points: 9,    jewelCost: 600,  image: "images/necklace-valakas.png"  },
+      antharas:  { label: "Antharas",     points: 9,    jewelCost: 350,  image: "images/earring-antharas.png"  },
+      baium:     { label: "Baium",        points: 9,    jewelCost: 300,  image: "images/ring-of-baium.png"     },
+      frintezza: { label: "Frintezza",    points: 4.5,  jewelCost: 120,  image: "images/frintezza-necklace.png"},
+      zaken:     { label: "Zaken",        points: 2.25, jewelCost: 100,  image: "images/zaken-earring.png"     },
+      antQueen:  { label: "Ant Queen",    points: 2.25, jewelCost: 90,   image: "images/ring-of-queen-ant.png" },
+      orfen:     { label: "Orfen",        points: 2.25, jewelCost: 40,   image: "images/earring-orfen.png"     },
+      core:      { label: "Core",         points: 2.25, jewelCost: 35,   image: "images/ring-of-core.png"      }
+    }
+  },
+  players: {
+    "DarkLord": {
+      activities: { siege: 4, valakas: 2, antharas: 1, frintezza: 2, zaken: 1, antQueen: 3, orfen: 1, core: 1 },
+      bought:     { orfen: 1, core: 1 }
+    },
+    "IronVeil": {
+      activities: { siege: 6, valakas: 2, antharas: 2, frintezza: 1, zaken: 2, antQueen: 2, orfen: 2, core: 1 },
+      bought:     { core: 1, orfen: 1 }
+    },
+    "ShadowBane": {
+      activities: { siege: 4, valakas: 3, antharas: 3, frintezza: 1, zaken: 1, antQueen: 2, orfen: 1, core: 1 },
+      bought:     { antQueen: 1 }
+    },
+    "CrimsonSeer": {
+      activities: { siege: 8, valakas: 1, antharas: 2, frintezza: 3, zaken: 1, antQueen: 1, orfen: 2, core: 3 },
+      bought:     { frintezza: 1 }
+    },
+    "NullEdge": {
+      activities: { siege: 5, valakas: 2, antharas: 2, frintezza: 2, zaken: 3, antQueen: 2, orfen: 2, core: 1 },
+      bought:     { zaken: 1 }
+    }
+  }
+};

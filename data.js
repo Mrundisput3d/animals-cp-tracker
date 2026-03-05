189ccf2b26ca79507ab45b3bd459563aa33250ed
Mrundisput3d <geogewrgiou@gmail.com>
2026-03-05 04:06:46 +0200

    Update data.js

--- data.js
+++ data.js
@@ -14,57 +14,18 @@ window.L2_DATA = {
     }
   },
   players: {
-    "Mrundisput3d": {
-      activities: { siege: 3, valakas: 2, antharas: 1, frintezza: 3, zaken: 6, antQueen: 9, orfen: 5, core: 4, baium: 3, newPlayerBonus: 0 },
-      bought: { zaken: 1 }
-    },
-    "Dreamcatcher": {
-      activities: { siege: 3, valakas: 2, antharas: 1, frintezza: 4, zaken: 3, antQueen: 7, orfen: 4, core: 2, baium: 3, newPlayerBonus: 0 },
-      bought: { }
-    },
-    "MONARXIDOS": {
-      activities: { siege: 3, valakas: 2, antharas: 1, frintezza: 2, zaken: 3, antQueen: 5, orfen: 4, core: 2, baium: 3, newPlayerBonus: 0 },
-      bought: { zaken: 1 }
-    },
-    "AgainstALL": {
-      activities: { siege: 3, valakas: 2, antharas: 1, frintezza: 6, zaken: 3, antQueen: 5, orfen: 0, core: 1, baium: 2, newPlayerBonus: 0 },
-      bought: { }
-    },
-    "HOLLISTER": { 
-      activities: { siege: 1, valakas: 2, antharas: 0, frintezza: 2, zaken: 3, antQueen: 4, orfen: 4, core: 2, baium: 3, newPlayerBonus: 0 },
-      bought: { }
-    },
-    "Fidempor12": {
-      activities: { siege: 3, valakas: 2, antharas: 1, frintezza: 1, zaken: 4, antQueen: 2, orfen: 3, core: 1, baium: 3, newPlayerBonus: 0 },
-      bought: { }
-    },
-    "IiSusHristos": {
-      activities: { siege: 2, valakas: 2, antharas: 0, frintezza: 2, zaken: 3, antQueen: 2, orfen: 3, core: 1, baium: 2, newPlayerBonus: 0 },
-      bought: { }
-    },
-    "GRP": {
-      activities: { siege: 2, valakas: 2, antharas: 0, frintezza: 0, zaken: 2, antQueen: 2, orfen: 1, core: 0, baium: 2, newPlayerBonus: 0 },
-      bought: { }
-    },
-    "Sammy": {
-      activities: { siege: 1, valakas: 0, antharas: 0, frintezza: 0, zaken: 0, antQueen: 0, orfen: 2, core: 0, baium: 0, newPlayerBonus: 1 },
-      bought: { }
-    },
-    "Psolemporas": {
-      activities: { siege: 1, valakas: 0, antharas: 0, frintezza: 0, zaken: 0, antQueen: 0, orfen: 0, core: 1, baium: 1, newPlayerBonus: 1 },
-      bought: { }
-    },
-    "Area51XxX": {
-      activities: { siege: 1, valakas: 1, antharas: 0, frintezza: 0, zaken: 3, antQueen: 2, orfen: 1, core: 0, baium: 1, newPlayerBonus: 0 },
-      bought: { }
-    },
-    "Lpan": {
-      activities: { siege: 1, valakas: 1, antharas: 0, frintezza: 0, zaken: 1, antQueen: 0, orfen: 2, core: 1, baium: 1, newPlayerBonus: 0 },
-      bought: { }
-    },
-    "SunnyLeone": {
-      activities: { siege: 0, valakas: 0, antharas: 0, frintezza: 0, zaken: 0, antQueen: 0, orfen: 0, core: 0, baium: 0, newPlayerBonus: 0 },
-      bought: { }
-    }
+    "Mrundisput3d": { activities: { siege: 3, valakas: 2, antharas: 1, frintezza: 3, zaken: 6, antQueen: 9, orfen: 5, core: 4, baium: 3, newPlayerBonus: 0 }, bought: { zaken: 1 } },
+    "Dreamcatcher": { activities: { siege: 3, valakas: 2, antharas: 1, frintezza: 4, zaken: 3, antQueen: 7, orfen: 4, core: 2, baium: 3, newPlayerBonus: 0 }, bought: { } },
+    "MONARXIDOS":   { activities: { siege: 3, valakas: 2, antharas: 1, frintezza: 2, zaken: 3, antQueen: 5, orfen: 4, core: 2, baium: 3, newPlayerBonus: 0 }, bought: { zaken: 1 } },
+    "AgainstALL":   { activities: { siege: 3, valakas: 2, antharas: 1, frintezza: 6, zaken: 3, antQueen: 5, orfen: 0, core: 1, baium: 2, newPlayerBonus: 0 }, bought: { } },
+    "HOLLISTER":    { activities: { siege: 1, valakas: 2, antharas: 0, frintezza: 2, zaken: 3, antQueen: 4, orfen: 4, core: 2, baium: 3, newPlayerBonus: 0 }, bought: { } },
+    "Fidempor12":   { activities: { siege: 3, valakas: 2, antharas: 1, frintezza: 1, zaken: 4, antQueen: 2, orfen: 3, core: 1, baium: 3, newPlayerBonus: 0 }, bought: { } },
+    "IiSusHristos": { activities: { siege: 2, valakas: 2, antharas: 0, frintezza: 2, zaken: 3, antQueen: 2, orfen: 3, core: 1, baium: 2, newPlayerBonus: 0 }, bought: { } },
+    "GRP":          { activities: { siege: 2, valakas: 2, antharas: 0, frintezza: 0, zaken: 2, antQueen: 2, orfen: 1, core: 0, baium: 2, newPlayerBonus: 0 }, bought: { } },
+    "Sammy":        { activities: { siege: 1, valakas: 0, antharas: 0, frintezza: 0, zaken: 0, antQueen: 0, orfen: 2, core: 0, baium: 0, newPlayerBonus: 1 }, bought: { } },
+    "Psolemporas":  { activities: { siege: 1, valakas: 0, antharas: 0, frintezza: 0, zaken: 0, antQueen: 0, orfen: 0, core: 1, baium: 1, newPlayerBonus: 1 }, bought: { } },
+    "Area51XxX":    { activities: { siege: 1, valakas: 1, antharas: 0, frintezza: 0, zaken: 3, antQueen: 2, orfen: 1, core: 0, baium: 1, newPlayerBonus: 0 }, bought: { } },
+    "Lpan":         { activities: { siege: 1, valakas: 1, antharas: 0, frintezza: 0, zaken: 1, antQueen: 0, orfen: 2, core: 1, baium: 1, newPlayerBonus: 0 }, bought: { } },
+    "SunnyLeone":   { activities: { siege: 0, valakas: 0, antharas: 0, frintezza: 0, zaken: 0, antQueen: 0, orfen: 0, core: 0, baium: 0, newPlayerBonus: 0 }, bought: { } }
   }
 };
